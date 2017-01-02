84a3127a00e865e5ecbe9432eec4390e09eef9ab
johannes.willmann@uni-konstanz.de <ChurFirst8>
2017-01-02 15:53:06 +0100

    final

---
+++ python/scrape_wiki_table.py
@@ -0,0 +1,28 @@
+"""
+This script scrapes the 'Importance of Religion' table from wikipedia.
+The original data is from gallup but is not accessible anymore.
+"""
+
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import re
+
+source_code = requests.get('https://en.m.wikipedia.org/wiki/Importance_of_religion_by_country')
+soup = BeautifulSoup(source_code.content)
+religiondatasaved=""
+tables = soup.find_all('table', class_="wikitable")
+
+table = tables[0]
+
+
+table
+
+for record in table.findAll('tr'):
+    religiondata=""
+    for data in record.findAll('td'):
+        religiondata = religiondata + "," + data.text
+        print(religiondata)
+    religiondatasaved = religiondatasaved + "\n" + religiondata[1:]
+
+
